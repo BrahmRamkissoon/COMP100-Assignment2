07f42300b91ba9c9975c968c448586dc84d95f51
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2015-10-12 11:40:30 -0400

    Setup file header

---
+++ Assignment2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Filename: Program.cs
+// Author: Brahm Ramkissoon
+// Created Date  (dd/mm/yyyy): 12/10/2015
+// Description: COMP100 Assignment 2 Part 1
+/* Write a console-based program that accepts a user's message and determines whether it is
+** short enough for a social networking service that does not accept messages of more than 140
+** characters. The pgroam should provide output that tells teh user if the message is accepted or
+** not. [Hint: if the user's input is assigned to the variable message then message.Length will 
+** provide the number of characters in the input]
+((4 Marks: Functionality)*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main ( string [] args )
+        {
+        }
+    }
+}
